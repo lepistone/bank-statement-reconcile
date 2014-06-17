70c2f37b4a0d3250ddf4dda56471b5499bc2cceb
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2014-06-17 18:11:26 +0200

    [FIX] optimize new rule

--- account_advanced_reconcile_bank_statement/__init__.py
+++ account_advanced_reconcile_bank_statement/__init__.py
@@ -19,5 +19,4 @@
 ##############################################################################
 
 from . import easy_reconcile
-from . import base_advanced_reconciliation
 from . import advanced_reconciliation
